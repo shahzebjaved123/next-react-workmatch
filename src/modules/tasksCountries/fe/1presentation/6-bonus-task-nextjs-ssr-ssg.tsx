8ffe9c26cd41a8fd974b1c9740f4ec190cbaa691
shahzeb javed <shahzebjaved23@gmail.com>
2022-10-19 12:09:50 +0500

    adds bonus task

--- src/modules/tasksCountries/fe/1presentation/6-bonus-task-nextjs-ssr-ssg.tsx
+++ src/modules/tasksCountries/fe/1presentation/6-bonus-task-nextjs-ssr-ssg.tsx
@@ -1,7 +1,17 @@
 /*
 Bonus Task:
  1. Please explain the methods getStaticPaths() and getStaticProps() for next.js pages
+
+ getStaticPaths, needs to return all the path that need to be generated at build time, 
+ This method will run once on server side, and generate all the paths. 
+
+ getStaticsProps, need to return the props, needed to render a page. 
+ The static pages will be pre-rendered, and this method can be used to provide props for that. 
+
  2. Please explain how to fetch the routes for dynamic paths
+
+ The nextjs uses the folder structuring generate the dynamic routes. 
+ the dynamic routes are generated using the params insde bracket i.e [code].tsx conventions. Like we use for country details page. 
 */
 
 
